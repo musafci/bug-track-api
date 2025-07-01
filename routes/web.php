0adc877ad1bab0518e115171ccce4fd0954684e1
Md Abu Musa <musa.fci@gmail.com>
2025-07-02 00:06:16 +0600

    Implement authentication and authorization functionality commit#02

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Add a login route for web authentication (if needed)
+Route::get('/login', function () {
+    return redirect('/');
+})->name('login');
